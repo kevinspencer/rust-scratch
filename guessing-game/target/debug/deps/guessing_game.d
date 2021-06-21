b79569a9aa50be100a18ee6df6ddcbd84b43d6da
Kevin Spencer <kevin@kevinspencer.org>
2021-06-21 15:26:56 -0700

    and so it begins

---
+++ guessing-game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+/Users/kevin/dev/rust-scratch/guessing-game/target/debug/deps/guessing_game: src/main.rs
+
+/Users/kevin/dev/rust-scratch/guessing-game/target/debug/deps/guessing_game.d: src/main.rs
+
+src/main.rs:
---
+++ guessing-game/target/debug/guessing-game.d
@@ -0,0 +1,1 @@
+/Users/kevin/dev/rust-scratch/guessing-game/target/debug/guessing-game: /Users/kevin/dev/rust-scratch/guessing-game/src/main.rs
